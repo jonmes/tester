7d50f74a1c514e080798d8823cbd695a06a6613a
jonmes <mesfinjohn@rocketmail.com>
2022-08-01 13:25:35 +0300

    removing nitro

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,9 +151,6 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxtjs/color-mode", "@vueuse/nuxt"],
   css: ["@/assets/css/main.css"],
-  nitro: {
-    preset: 'node-server'
-  },
   build: {
     postcss: {
       postcssOptions: require("./postcss.config.js"),
